a993e627821aa50baa172304e25a8dbedd6f4197
steingartpeter <steingart.p2@gmail.com>
2023-02-12 15:46:19 +0100

    2023-02-12 Finished 001

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'emailform';
+  email: string = '';
+
+  constructor() {
+    // setInterval(() => {
+    //   this.email += 'a';
+    // }, 1000);
+  }
+
+  onSubmit() {
+    console.log(`Form submitted => ${this.email}`);
+  }
 }
